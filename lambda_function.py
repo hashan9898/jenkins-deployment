9d68d2a38f81ce5f0d4d12d8445a53869202087b
hashan9898 <94815315+hashan9898@users.noreply.github.com>
2022-11-10 10:14:14 +0530

    first

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
-import json
+import boto3
+region = 'ap-south-1'
+instances = ['i-02083b819c731ec2d']
+ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Middleware!')
-    }
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
